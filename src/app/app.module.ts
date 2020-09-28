30189d4cbf6ff7432726dd50247a3549bdf90239
Ashok Shinde <shindeashok.as.02@gmail.com>
2020-09-28 15:02:11 +0530

    project clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterSuccessComponent } from './auth/register-success/register-success.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    RegisterComponent,
-    LoginComponent,
-    RegisterSuccessComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
